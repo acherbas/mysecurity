1c067b5c238cc4e01a947de5015487221a5bb175
Saber Mnasri <saber.mnasri@hotmail.com>
2021-12-09 13:43:11 +0100

    first commit

---
+++ src/test/java/com/macrosol/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macrosol.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
